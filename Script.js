78084de973953fc67b01308586898e6dc936b364
Lance Campos <lcampos20058@columbushs.com>
2020-02-01 16:47:52 -0500

    fix format

--- Script.js
+++ Script.js
@@ -1,37 +1,16 @@
 
-const cvs = document.getElementById('snake');
-const ctx = cvs.getContext('2d');
+let h = document.getElementById("hour")
+let m = document.getElementById("mintue")
+let s = document.getElementById("second")
 
-// Images
-const ground = new Image();
-ground.src = "img/ground.png";
+let hours = getHours();
+let minutes = getMinutes();
+let seconds = getSeconds();
 
-const foodImg = new Image();
-foodImg.src = "img/food.png";
-
-// Audio
-let dead = new Audio();
-let eat = new Audio();
-let up = new Audio();
-let right = new Audio();
-let left = new Audio();
-let down = new Audio();
-
-dead.src = "audio/dead.mp3";
-eat.src = "audio/dead.mp3";
-up.src = "audio/walk.mp3";
-right.src = "audio/walk.mp3";
-left.src = "audio/walk.mp3";
-down.src = "audio/walk.mp3";
-
-// Unit 
-const box = 32;
-
-
-
-
-
-function draw(){
-    ctx.drawimage(ground,0,0)
+function time(){
+    h.innerHTML = hours;
+    m.innerHTML = minutes;
+    s.innerHTML = seconds;
 }
-let game = setInterval (draw, 100)
+
+time();
