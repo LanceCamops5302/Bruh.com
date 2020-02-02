007e6905fad1358dd902228117518a4c99f8a747
Lance Campos <lcampos20058@columbushs.com>
2020-02-02 15:55:44 -0500

    almost done

--- Script.js
+++ Script.js
@@ -1,16 +1,36 @@
 
 let h = document.getElementById("hour")
-let m = document.getElementById("mintue")
-let s = document.getElementById("second")
+let m = document.getElementById("min")
+let s = document.getElementById("sec")
 
-let hours = getHours();
-let minutes = getMinutes();
-let seconds = getSeconds();
+
+let date = new Date();
+let hours = date.getHours();
+let minut = date.getMinutes();
+let secon = date.getSeconds();
+
 
 function time(){
-    h.innerHTML = hours;
-    m.innerHTML = minutes;
-    s.innerHTML = seconds;
-}
+   /*if(hours == 1||2||3||4||5||6||7||8||9||0){
+        hours = `0${hours}`
+    }; 
 
-time();
+    if (minut == 1||2||3||4||5||6||7||8||9||0){
+        minut = `0${minut}`
+    };
+
+    if(secon == 1||2||3||4||5||6||7||8||9||0){
+        secon = `0${secon}`
+    }; 
+*/
+    h.innerHTML = `${hours}:`;
+    m.innerHTML = `${minut}:`;
+    s.innerHTML = secon;
+};
+
+
+function timer(){
+setInterval(time(), 500);
+};
+
+timer();
